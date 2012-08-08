47eca901ff6d343d964d1602196d13b51f47d063
Joachim Kudish <joachim.kudish@gmail.com>
2012-08-08 11:37:28 -0700

    unresolved posts widgets: cleanup JS
    cleanup the JavaScrip in the unresolved posts widget:
    
    * cache the `$(this)` selector
    * use the `$` sign for the parent var, since it represents a dom object
    * spacing/readability improvements
    
    see #18

--- php/p2_resolved-posts-show-unresolved-posts-widget.php
+++ php/p2_resolved-posts-show-unresolved-posts-widget.php
@@ -56,15 +56,19 @@
 		}
 		</style>
 		<script type="text/javascript">
-			jQuery(document).ready(function(){
+			jQuery(document).ready(function($){
 
-				jQuery('.p2-resolved-posts-pagination-link').click(function(){
-					var parent = jQuery(this).closest('.p2-resolved-posts-show-unresolved-posts');
-					var first_post = parseInt( parent.find('.p2-resolved-posts-first-post').html() );
-					var last_post = parseInt( parent.find('.p2-resolved-posts-last-post').html() );
-					var total_posts = parseInt( parent.find('.p2-resolved-posts-total-posts').html() )
-					var posts_per_page = parseInt( parent.find('.p2-resolved-posts-ppp').html() );
-					if ( jQuery(this).hasClass('p2-resolved-posts-previous-posts') ) {
+				$('.p2-resolved-posts-pagination-link').click(function(){
+
+					var $this = $(this);
+
+					var $parent = $this.closest( '.p2-resolved-posts-show-unresolved-posts' );
+					var first_post = parseInt( $parent.find( '.p2-resolved-posts-first-post' ).html() );
+					var last_post = parseInt( $parent.find( '.p2-resolved-posts-last-post' ).html() );
+					var total_posts = parseInt( $parent.find( '.p2-resolved-posts-total-posts' ).html() )
+					var posts_per_page = parseInt( $parent.find ('.p2-resolved-posts-ppp' ).html() );
+
+					if ( $this.hasClass( 'p2-resolved-posts-previous-posts' ) ) {
 						// Don't paginate if we're at the first item
 						if ( first_post <= 1 )
 							return false;
@@ -72,7 +76,7 @@
 						first_post = first_post - posts_per_page;
 						last_post = first_post + ( posts_per_page - 1);
 
-					} else if ( jQuery(this).hasClass('p2-resolved-posts-next-posts') ) {
+					} else if ( $this.hasClass( 'p2-resolved-posts-next-posts' ) ) {
 						// Don't paginate if we're at the last item
 						if ( last_post >= total_posts )
 							return false;
@@ -83,16 +87,18 @@
 							last_post = total_posts;
 
 					}
+
 					// Show posts based on our pagination counter
-					parent.find( 'ul li' ).removeClass('active').addClass('hidden');
-					parent.find( 'ul li' ).each(function( index, value ) {
+					$parent.find( 'ul li' ).removeClass( 'active' ).addClass( 'hidden' );
+					$parent.find( 'ul li' ).each(function( index, value ) {
 						if ( ( index + 1 ) >= first_post && ( index + 1 ) <= last_post )
-							jQuery(this).addClass('active').removeClass('hidden');
+							$(this).addClass( 'active' ).removeClass( 'hidden') ;
 
 					});
+
 					// Reset the pagination
-					parent.find('.p2-resolved-posts-first-post').html( first_post );
-					parent.find('.p2-resolved-posts-last-post').html( last_post );
+					$parent.find( '.p2-resolved-posts-first-post' ).html( first_post );
+					$parent.find( '.p2-resolved-posts-last-post' ).html( last_post );
 					return false;
 				});
 
